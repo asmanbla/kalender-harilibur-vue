3d95990f0bd1181b7f97a3d854adfd233db32005
asmanbla <asmanabila03@gmail.com>
2025-07-04 13:30:05 +0700

    vite.config.js outDir : 'dist'

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
   build: {
+    outDir: 'dist', 
     rollupOptions: {
       input: {
-        daftarHariLibur: "harilibur.html",
-        hariLiburBulan8: "harilibur8.html",
-        hariLiburCalendar: "hariliburcalendar.html",
+        daftarHariLibur: 'harilibur.html',
+        hariLiburBulan8: 'harilibur8.html',
+        hariLiburCalendar: 'hariliburcalendar.html',
       }
     }
   }
